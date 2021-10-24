b72303b644aa34de05bd94ac018435fd57f9d3ae
Lucas Templeton <templetl99@gmail.com>
2021-10-23 19:32:10 -0500

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name:"Lucas",
+    streetAddress:"1street"
+};
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress){
+    const newEmployee = {...employee};
+    newEmployee[name] = streetAddress;
+    return newEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress){
+    employee[name]=streetAddress
+    return employee
+}
+function deleteFromEmployeeByKey(employee, name){
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee.name;
+    return employee
+}
